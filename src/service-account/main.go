e45cda5dd675105f8ce06a1a9c90e23e961db224
zhengguo07q <zg@DESKTOP-7FF2FJ7>
2018-01-26 21:05:26 +0800

    change dir
    Signed-off-by: zhengguo07q <zg@DESKTOP-7FF2FJ7>

---
+++ src/service-account/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open("mysql", "root:hjwan817@/account?charset=utf8")
+	if err == nil {
+		DB = db
+		db.LogMode(true)
+		db.AutoMigrate(&Page{}, &Post{}, &Tag{}, &PostTag{}, &User{}, &Comment{}, &Subscriber{}, &Link{})
+		db.Model(&PostTag{}).AddUniqueIndex("uk_post_tag", "post_id", "tag_id")
+		return db, err
+	}
+	//ddfgs
+	return nil, err
+}
+ 
